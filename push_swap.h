9d9d0d1b2dffee70b7ff77ef46a0e9fd6638f8fe
Stephanie Maire <stmaire@z1r11p1.42lyon.fr>
2026-01-02 09:12:47 +0100

    handle_error first

--- push_swap.h
+++ push_swap.h
@@ -6,16 +6,36 @@
 /*   By: stmaire <stmaire@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/30 14:26:35 by stmaire           #+#    #+#             */
-/*   Updated: 2025/12/30 14:35:56 by stmaire          ###   ########.fr       */
+/*   Updated: 2025/12/31 13:13:20 by stmaire          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 #define	PUSH_SWAP_H
 
-#include "libft.h"
-#include "ft_printf.h"
+#include "libft/libft.h"
+#include "printf/ft_printf.h"
 #include <unistd.h>
 #include <stdlib.h>
+#include <limits.h>
+
+//-------------------------------STRUCTURE-------------------------------
+typedef struct s_stack_node
+{
+	/* data to complete */
+	int					number;
+	struct s_stack_node	*prev;
+	struct s_stack_node	*next;
+}						t_stack_node;
+/* -------------------------------PARSING---------------------------------*/
+//***build***/
+char			**put_args_in_array(int argc, char **argv);
+t_stack_node	*build_stack(char **tab);
+//***check***/
+int				check_args_syntax(char **tab);
+int 			check_args_overflow(long i);
+int				check_args_doubles(t_stack_node *node, int nb_to_check);
+//***free***/
+t_stack_node 	*free_if_error(t_stack_node *stack,char **tab);
 
 #endif
