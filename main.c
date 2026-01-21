12eed50624085da0f19cea57fe375c3954fa2d20
Stephanie Maire <stmaire@z2r11p4.42lyon.fr>
2026-01-21 10:24:11 +0100

    handle flags OK

--- main.c
+++ main.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stmaire <stmaire@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/21 10:19:30 by stmaire           #+#    #+#             */
+/*   Updated: 2026/01/21 10:23:20 by stmaire          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
 #include <stdio.h> 
 
@@ -15,56 +27,61 @@ static void	choose_method(int score)
 	else
 		printf("Error\n"); // print_error
 	if (score == 100 || score == 111 || score == 103
-			|| score == 105 || score == 107)
+		|| score == 105 || score == 107)
 		printf("Benchmark will be visible\n"); // bench
 }
-// char *truncate(char *str)
-// {
-// 	char *str_without_flags;
-// 	int	i;
-// 	int	score;
-	
-// 	i = 0;
-// 	score = flag_selector(str);
-// 	if (score == -1)
-// 		return(str);
-// 	else 
-// 	{
-// 		on va au bout de la chaine
-// 		on recule while str[i] != ' ' && str[i -1] != minuscule;
-// 		str_without_flags = ft_strdup(str + i);
-// 		free(str);
-// 		return(str_without_flags);
-// 	}
 
-// }
+static	char *ft_truncate(char *str)
+{
+	char	*str_without_flags;
+	int		i;
+	int		score;
+
+	if (!str)
+		return (NULL);
+	score = flag_selector(str);
+	if (score == -1)
+		return (str);
+	else
+	{
+		i = ft_strlen(str) - 1;
+		while (i > 1)
+		{
+			if (str[i] == ' ' && (str[i -1] >= 97 && str[i - 1] <= 122))
+			{
+				str_without_flags = ft_strdup(str + i + 1);
+				free(str);
+				return (str_without_flags);
+			}
+			i--;
+		}
+		return (str);
+	}
+}
 
 int	main(int argc, char **argv)
 {
-	// t_stack_node	*stack_a;
-	// t_stack_node	*stack_b;
-	// char			**tab;
+	t_stack_node	*stack_a;
+	t_stack_node	*stack_b;
+	char			**tab;
 	char			*big_str;
 	int				score;
-	
+
 	if (argc < 2)
 		return (0);
-
 	big_str = from_args_to_big_str(argc, argv);
 	score = flag_selector(big_str);
 	choose_method(score); // seulement apres les constructions des stacks parce qu on en a besoin	
-	
-	// big_str = truncate(big_str);
-	// tab = put_args_in_array(big_str);
-	
+	big_str = ft_truncate(big_str);
+	tab = put_args_in_array(big_str);
 	//big_str peut-être a free plutôt ici?????
-	// stack_a = NULL;
-	// stack_b = NULL;
-	// stack_a = build_stack(tab);
-	// if (!stack_a)
-	// 	return (1);
-	// range_based_sort(&stack_a, &stack_b); 
-	// free_stack(&stack_a);
-	// free_stack(&stack_b);
+	stack_a = NULL;
+	stack_b = NULL;
+	stack_a = build_stack(tab);
+	if (!stack_a)
+		return (1);
+	range_based_sort(&stack_a, &stack_b);
+	free_stack(&stack_a);
+	free_stack(&stack_b);
 	return (0);
 }
